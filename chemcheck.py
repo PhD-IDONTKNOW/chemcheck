e2401e905fa7503958c8ff6cd5770424f3c5fa5c
Egor Mustaev <80110272+PhD-IDONTKNOW@users.noreply.github.com>
2021-04-04 19:36:57 +0500

    Start working with [-mp] flag. Functions doc added.
    In this update was added melting and boiling pointf for alements up to Argon. Code more elegance.  Documentation for function was added. Help page was updated.

--- chemcheck.py
+++ chemcheck.py
@@ -1,80 +1,110 @@
 from elements_list import atom_masses
 
 
-def flags(user_in):
-    if '-info' in user_in or '-i' in user_in:  # find flags in input text
-        if user_in[:2] in atom_masses and user_in[2] == ' ':
-            print(atom_masses[user_in[:2]][1])
-        elif user_in[0] in atom_masses and user_in[1] == ' ':
-            print(atom_masses[user_in[0]][1])
-        else:
-            print('This element does not exist!')
-    elif '-melting_point' in user_in or '-mp' in user_in:
-        print('Elements melting point was not add yet, but water melt at 0 degrees Celsius')
-    elif '-help' in user_in or '-h' in user_in:
+def flags(user_input):
+    """
+    This void function need if user use flags in commands.
+
+    :param user_input:
+    :return: Nothing
+    """
+    if '-h' in user_input:
         print("""
     _____Help Page_____
 
     To calculate molar mass just print compound without flags (note that register are import!)
         for example: (NH4)2Cr2O7    ->      252.061
                      h2o            ->      error
-                    ----------------------------
-    [-info] or [-i]             show information about element (not compound!)
-                                    for example: Hg -info -> Mercury is liquid metal
-                    ----------------------------
-    [-melting_point] or [-mp]   show melting point of element (not compound!)
-                                    for example: Fe -mp -> Iron melt at 1812 degrees Kelvin or 1538.85 degrees Celsius
-                    ----------------------------
-    [-help] or [-h]             show this message
-                    ----------------------------
-    [exit]                      close
+                ----------------------------
+    [-i]                 show information about element (not compound!)
+                            for example: Hg -i -> Mercury is liquid metal
+                ----------------------------
+    [-mp]                show melting point of element (not compound!)
+                            for example: Fe -mp -> Iron melt at 1812 degrees Kelvin or 1538.85 degrees Celsius
+                ____________________________
+    [-u] + [dK/dC/dF]    change unit system
+                            for example: Cl -mp -u dK -> Chlorine melts at 172.2 degrees Kelvin
+                ----------------------------
+    [-h]                 show this message
+                ----------------------------
+    [exit]               close
             """)
+    if '-u' in user_input:
+        print('Now only Kelvin scale support')
+    if '-i' in user_input:  # find flags in input text
+        if user_input[:2] in atom_masses and user_input[2] == ' ':
+            print(atom_masses[user_input[:2]][1])
+        elif user_input[0] in atom_masses and user_input[1] == ' ':
+            print(atom_masses[user_input[0]][1])
+        else:
+            print('This element does not exist!')
+    elif '-mp' in user_input:
+        if user_input[:2] in atom_masses and user_input[2] == ' ':
+            print(f'{atom_masses[user_input[:2]][1]} melts at {atom_masses[user_input[:2]][2]} degrees Kelvin')
+        elif user_input[0] in atom_masses and user_input[1] == ' ':
+            print(f'{atom_masses[user_input[0]][1]} melts at {atom_masses[user_input[0]][2]} degrees Kelvin')
+        else:
+            print('This element does not exist!')
     else:
-        print('Unknown flag. Use [-help] to get help.')
+        print('Unknown flag. Use [-h] to get help.')
 
 
-def molar_calc(no_brackets):  # this function find molar mass of formatted compound without brackets
+def molar_calc(without_brackets):
+    """
+    This function find molar mass of compound. Don't use if compound has brackets!
+        for example: CaSO4 -> 136.137
+            Ca(NO3)3 -> may cause error
+    :param without_brackets:
+    :return: molar mass
+    """
     f_molar_mass = 0
     # Check if compound could be
-    if not no_brackets[:1] in atom_masses and not no_brackets[:2] in atom_masses:
+    if not without_brackets[:1] in atom_masses and not without_brackets[:2] in atom_masses:
         print("This compound does not exist! ")
     else:
-        for k in range(len(no_brackets)):
+        for k, char in enumerate(without_brackets):
             # if element in compound consist of 2 letters (like Li, He, Fe etc.)
-            if no_brackets[k:2 + k] in atom_masses:
-                if no_brackets[k + 2:3 + k] in list('123456789'):  # check if element has index
-                    f_molar_mass += int(no_brackets[k + 2:3 + k]) * atom_masses[no_brackets[k:2 + k]][0]
+            if without_brackets[k:k + 2] in atom_masses:
+                if without_brackets[k + 2:3 + k] in list('123456789'):  # check if element has index
+                    f_molar_mass += int(without_brackets[k + 2:k + 3]) * atom_masses[without_brackets[k:k + 2]][0]
                 else:
-                    f_molar_mass += atom_masses[no_brackets[k:2 + k]][0]
-                k += 1
-            elif no_brackets[
-                 k:1 + k] in atom_masses:  # if element in compound consist of 1 letter (like H, C, N, O etc.)
-                if no_brackets[k + 1:2 + k] in list('123456789'):  # check if element has index
-                    f_molar_mass += int(no_brackets[k + 1:2 + k]) * atom_masses[no_brackets[k:1 + k]][0]
+                    f_molar_mass += atom_masses[without_brackets[k:k + 2]][0]
+                continue
+            elif char in atom_masses:  # if element in compound consist of 1 letter (like H, C, N, O etc.)
+                if without_brackets[k + 1:k + 2] in list('123456789'):  # check if element has index
+                    f_molar_mass += int(without_brackets[k + 1:k + 2]) * atom_masses[char][0]
                 else:
-                    f_molar_mass += atom_masses[no_brackets[k:1 + k]][0]
+                    f_molar_mass += atom_masses[char][0]
     return round(f_molar_mass, 3)  # return rounded to 3 digits molar mass of formatted compound
 
 
-# this function separate input compound to bracket part and un bracket part
-def separator(no_flags):
+def separator(with_brackets):
+    """
+    This function separate compound to brackets part and un brackets part. Don't use if no brackets in compound!
+        for example: Fe(NO3)3 -> ('Fe' , 'NO3', 3)
+            NaCl -> cause error
+    :param with_brackets:
+    :return: out brackets part, brackets part, brackets coefficient
+    """
     # find parentheses
-    first_bracket, second_bracket = no_flags.find('('), no_flags.find(')')
+    first_bracket, second_bracket = with_brackets.find('('), with_brackets.find(')')
     # check is coefficient does exist
-    is_coef = len(no_flags) > second_bracket + 1 and no_flags[second_bracket + 1] in '123456789'
+    is_coef = len(with_brackets) > second_bracket + 1 and with_brackets[second_bracket + 1] in '123456789'
     # save bracket part
-    brackets_part = no_flags[first_bracket + 1:second_bracket]
+    brackets_part = with_brackets[first_bracket + 1:second_bracket]
     # search coefficient
-    coefficient = no_flags[second_bracket + 1] if is_coef else 1
+    coefficient = with_brackets[second_bracket + 1] if is_coef else 1
     # search out bracket part
-    out_brackets_part = no_flags[:first_bracket] + no_flags[second_bracket + 2:] if \
+    out_brackets_part = with_brackets[:first_bracket] + with_brackets[second_bracket + 2:] if \
         is_coef else \
-        no_flags[:first_bracket] + no_flags[second_bracket + 1:]
+        with_brackets[:first_bracket] + with_brackets[second_bracket + 1:]
     return out_brackets_part, brackets_part, int(coefficient)
 
 
 while True:
-    compound = input("Print compound symbol or [-help] to get help: ")  # get input compound
+    compound = input('''
+___Warning! [-mp] cause program crash!___
+Print compound symbol or [-h] to get help: ''')  # get input compound
 
     if compound == 'exit':  # exit function
         break
